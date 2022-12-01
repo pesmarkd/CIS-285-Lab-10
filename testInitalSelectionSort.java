6f86e7cec84098a7198615b91f6a8a6654babd9e
Devin Pesmark <pesmarkd@umich.edu>
2022-12-01 16:44:35 -0500

    All passed

--- testInitalSelectionSort.java
+++ testInitalSelectionSort.java
@@ -1,6 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.Assert;
 
 class testInitalSelectionSort {
 
